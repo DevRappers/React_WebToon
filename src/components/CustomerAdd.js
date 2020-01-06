d27b43951d084e09959671c6429487dc318497e1
DevRappers <devrappers@gmail.com>
2020-01-06 10:22:41 +0900

    컴포넌트 디자인

--- src/components/CustomerAdd.js
+++ src/components/CustomerAdd.js
@@ -1,8 +1,22 @@
 import React, { useState } from 'react';
 import { post } from 'axios';
 import useInput from '../hooks/useInput';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
 
-function CustomerAdd({ stateRefresh }) {
+const styles = (theme) => ({
+	hidden: {
+		display: 'none'
+	}
+});
+
+function CustomerAdd({ stateRefresh, classes }) {
+	const [ open, setOpen ] = useState(false);
 	const [ file, setFile ] = useState(null);
 	const [ filename, setFilename ] = useState('');
 	const name = useInput('');
@@ -26,6 +40,20 @@ function CustomerAdd({ stateRefresh }) {
 		return post(url, formData, config);
 	};
 
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClickClose = () => {
+		setOpen(false);
+		setFile(null);
+		setFilename('');
+		name.setValue('');
+		createday.setValue('');
+		genre.setValue('');
+		author.setValue('');
+	};
+
 	const handleFormSubmit = (e) => {
 		e.preventDefault();
 		addCustomer().then((response) => {
@@ -36,9 +64,9 @@ function CustomerAdd({ stateRefresh }) {
 		setFilename('');
 		name.setValue('');
 		createday.setValue('');
-
 		genre.setValue('');
 		author.setValue('');
+		setOpen(false);
 	};
 
 	const handleFileChange = (e) => {
@@ -47,21 +75,67 @@ function CustomerAdd({ stateRefresh }) {
 	};
 
 	return (
-		<form onSubmit={handleFormSubmit}>
-			<h1>웹툰 추가</h1>
-			웹툰 이미지 : <input type="file" name="file" file={file} value={filename} onChange={handleFileChange} />
-			<br />
-			웹툰명 : <input type="text" name="name" value={name.value} onChange={name.onChange} />
-			<br />
-			게시일 : <input type="text" name="createday" value={createday.value} onChange={createday.onChange} />
-			<br />
-			장르 : <input type="text" name="genre" value={genre.value} onChange={genre.onChange} />
-			<br />
-			작가명 : <input type="text" name="author" value={author.value} onChange={author.onChange} />
-			<br />
-			<button type="submit">추가하기</button>
-		</form>
+		<div>
+			<Button variant="contained" color="primary" onClick={handleClickOpen}>
+				웹툰 추가하기
+			</Button>
+			<Dialog open={open} onClose={handleClickClose}>
+				<DialogTitle>웹툰추가</DialogTitle>
+				<DialogContent>
+					<input
+						className={classes.hidden}
+						accept="image/*"
+						id="raised-button-file"
+						type="file"
+						file={file}
+						value={filename}
+						onChange={handleFileChange}
+					/>
+					<label htmlFor="raised-button-file">
+						<Button varient="contained" color="primary" component="span" name="file">
+							{filename === '' ? '웹툰 이미지 선택' : filename}
+						</Button>
+					</label>
+					<br />
+					<TextField label="이름" type="text" name="name" value={name.value} onChange={name.onChange} />
+					<br />
+					<TextField
+						label="시작일"
+						type="text"
+						name="createday"
+						value={createday.value}
+						onChange={createday.onChange}
+					/>
+					<br />
+					<TextField label="장르" type="text" name="genre" value={genre.value} onChange={genre.onChange} />
+					<br />
+					<TextField label="작가명" type="text" name="author" value={author.value} onChange={author.onChange} />
+				</DialogContent>
+				<DialogActions>
+					<Button variant="contained" color="primary" onClick={handleFormSubmit}>
+						추가
+					</Button>
+					<Button variant="outlined" color="primary" onClick={handleClickClose}>
+						닫기
+					</Button>
+				</DialogActions>
+			</Dialog>
+		</div>
+		// <form onSubmit={handleFormSubmit}>
+		// 	<h1>웹툰 추가</h1>
+		// 	웹툰 이미지 : <input type="file" name="file" file={file} value={filename} onChange={handleFileChange} />
+		// 	<br />
+		// 	웹툰명 : <input type="text" name="name" value={name.value} onChange={name.onChange} />
+		// 	<br />
+		// 	게시일 : <input type="text" name="createday" value={createday.value} onChange={createday.onChange} />
+		// 	<br />
+		// 	장르 : <input type="text" name="genre" value={genre.value} onChange={genre.onChange} />
+		// 	<br />
+		// 	작가명 : <input type="text" name="author" value={author.value} onChange={author.onChange} />
+		// 	<br />
+		// 	<button type="submit">추가하기</button>
+		// </form>
 	);
 }
 
-export default CustomerAdd;
+export default withStyles(styles)(CustomerAdd);
--- src/components/CustomerDelete.js
+++ src/components/CustomerDelete.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 function CustomerDelete({ stateRefresh, id }) {
+	const [ open, setOpen ] = useState(false);
+
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClickClose = () => {
+		setOpen(false);
+	};
+
 	const deleteWebtoon = (id) => {
 		const url = '/api/webtoons/' + id;
 		fetch(url, {
@@ -9,13 +25,25 @@ function CustomerDelete({ stateRefresh, id }) {
 		stateRefresh();
 	};
 	return (
-		<button
-			onClick={(e) => {
-				deleteWebtoon(id);
-			}}
-		>
-			삭제
-		</button>
+		<div>
+			<Button variant="contained" color="secondary" onClick={handleClickOpen}>
+				삭제
+			</Button>
+			<Dialog open={open} onClose={handleClickClose}>
+				<DialogTitle onClose={handleClickClose}>삭제 경고</DialogTitle>
+				<DialogContent>
+					<Typography gutterBottom>선택한 웹툰 정보가 삭제됩니다.</Typography>
+				</DialogContent>
+				<DialogActions>
+					<Button variant="contained" color="primary" onClick={() => deleteWebtoon(id)}>
+						삭제
+					</Button>
+					<Button variant="outlined" color="primary" onClick={handleClickClose}>
+						닫기
+					</Button>
+				</DialogActions>
+			</Dialog>
+		</div>
 	);
 }
 
